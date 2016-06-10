46fb528db0be48256d18b65317799f867cda3163
Tony Fabery <tfabery@gmail.com>
2016-06-10 09:38:11 -0700

    add generate an array with numbers from 1 to the inputted number and change multiples of 3 in the array to 'ping'

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//=========================BackEnd=========================
+var pingpong = function (num) {
+  var numArray = [];
+  for (i = 1; i <= num; i ++) {
+    numArray.push(i);
+  };
+  for (i = 0; i < numArray.length; i ++) {
+    if ((numArray[i] % 3) === 0) {
+      numArray.splice(i, 1, 'ping');
+    }
+  };
+  console.log(numArray);
+  return numArray;
+};
+//=========================FrontEnd========================
+$(function() {
+  $('.inputnum').submit(function(event) {
+    event.preventDefault();
+    var num = parseInt($('#inputnum').val());
+    pingpong(num);
+  });
+});
